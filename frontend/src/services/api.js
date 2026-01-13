deebc1cc035c65cbdd214d5015377cce8a4ec860
Atikesh Chouhan <atikeshchouhan7@gmail.com>
2026-01-13 17:51:32 +0530

    Frontend Bacend connection established

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://gigflow-g43e.onrender.com/api",
 });
 
 api.interceptors.request.use((config) => {
